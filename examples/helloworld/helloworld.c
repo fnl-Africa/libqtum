e2d62c3864008a30a92499eee829a39e2619bfda
Jordan Earls <earlz@earlz.net>
2019-01-10 19:23:57 -0700

    Add helloworld

---
+++ examples/helloworld/helloworld.c
@@ -0,0 +1,11 @@
+#include <qtum.h>
+
+int onCreate(){
+    qtumEventStringString("Hello World", "Contract creation");
+    return 0;
+}
+
+int main(){
+    qtumEventStringString("Hello World", "Execution Success!");
+    return 0;
+}
